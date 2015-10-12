4b535e98cb40d398f1a390f36b973d71da95c5c5
captiosus <royxu652@gmail.com>
2015-10-12 16:00:22 -0400

    login system

--- database.py
+++ database.py
@@ -12,15 +12,35 @@ c.execute("create table if not exists user (username text, password text, id int
 
 def addPost(body, postid, userid):
     c.execute("insert into post values ('{}', '{}', {});".format(body, postid, userid))
-    
+
 def addComment(body, commentid, replyid, userid):
     c.execute("insert into comment values ('{}', '{}', '{}', {});".format(body, commentid, replyid, userid))
-    
-def addUser(username, password, userid):
-    c.execute("insert into user values ('{}', '{}', {});".format(username, password, userid))
 
-addPost("blablabla","P1",3)
-addComment("very interesting","C1","P1",3)
-addUser("abc","password",3)
+def newUser(username, password):
+    conn = sqlite3.connect("user.db")
+    c = conn.cursor()
+    isTaken = "SELECT username FROM user WHERE username=:\"%s\" LIMIT 1" % (username)
+    c.execute(isTaken)
+    data = c.fetchone()
+    if data is None :
+        m = hashlib.sha224(password)
+        u = hashlib.sha224(username)
+        query = "INSERT INTO login VALUES (\"%s\", \"%s\", \"%s\")" % (username, m.hexdigest(), u.hexdigest())
+        c.execute(query)
+        conn.commit()
+        return True
+    return False
 
-conn.commit()
+def authenticate(username, password):
+    conn = sqlite3.connect("user.db")
+    c = conn.cursor()
+    m = hashlib.sha224(password).hexdigest()
+    query = "SELECT password FROM user WHERE username=\"%s\"" % (username)
+    c.execute(query)
+    s1 = c.fetchone()
+    if s1 == None:
+        return False
+    s2 = s1[0]
+    if s2 == m:
+        return True
+    return False
