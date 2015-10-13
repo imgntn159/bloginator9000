d90c9fdf8c807e0f72aada3af10982a2840bbea8
RJZheng1 <stuykid2016@yahoo.com>
2015-10-13 12:06:43 -0400

    Did some small things.

--- database.py
+++ database.py
@@ -1,23 +1,35 @@
 import sqlite3
 
-conn = sqlite3.connect("bloginator9000.db")
 
-c = conn.cursor()
-
-c.execute("create table if not exists post (body text, id text, userid integer)")
-
-c.execute("create table if not exists comment (body text, id text, replyid text, userid integer)")
-
-c.execute("create table if not exists user (username text, password text, id integer)")
+def makeTables():
+    conn = sqlite3.connect("bloginator9000.db")
+    c = conn.cursor()
+    c.execute("create table if not exists post (body text, id text, userid text)")
+    c.execute("create table if not exists comment (body text, id text, replyid text, userid text)")
+    c.execute("create table if not exists user (username text, password text, id text)")
+    conn.commit()
 
 def addPost(body, postid, userid):
+    conn = sqlite3.connect("bloginator9000.db")
+    c = conn.cursor()
     c.execute("insert into post values ('{}', '{}', {});".format(body, postid, userid))
+    conn.commit()
+    
+def deletePost(postid):
+    conn = sqlite3.connect("bloginator9000.db")
+    c = conn.cursor()
+    c.execute("SELECT p FROM post WHERE postid='{}' LIMIT 1".format(postid))
+    data = c.fetchone()
+    conn.commit()
 
 def addComment(body, commentid, replyid, userid):
+    conn = sqlite3.connect("bloginator9000.db")
+    c = conn.cursor()
     c.execute("insert into comment values ('{}', '{}', '{}', {});".format(body, commentid, replyid, userid))
-
+    conn.commit()
+    
 def newUser(username, password):
-    conn = sqlite3.connect("user.db")
+    conn = sqlite3.connect("bloginator9000.db")
     c = conn.cursor()
     isTaken = "SELECT username FROM user WHERE username=:\"%s\" LIMIT 1" % (username)
     c.execute(isTaken)
@@ -32,7 +44,7 @@ def newUser(username, password):
     return False
 
 def authenticate(username, password):
-    conn = sqlite3.connect("user.db")
+    conn = sqlite3.connect("bloginator9000.db")
     c = conn.cursor()
     m = hashlib.sha224(password).hexdigest()
     query = "SELECT password FROM user WHERE username=\"%s\"" % (username)
@@ -44,3 +56,5 @@ def authenticate(username, password):
     if s2 == m:
         return True
     return False
+
+makeTables()
