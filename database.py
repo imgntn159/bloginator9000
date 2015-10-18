3a43ec7d2ad6a46540d4561ec8cad32d54813123
RJZheng1 <stuykid2016@yahoo.com>
2015-10-18 19:30:35 -0400

    Hopefully titles for posts work.

--- database.py
+++ database.py
@@ -3,74 +3,56 @@ import sqlite3, hashlib
 def makeTables():
     conn = sqlite3.connect("bloginator9000.db")
     c = conn.cursor()
-    c.execute("create table if not exists post (body text, id text, userid text, timestamp DATETIME)")
-    c.execute("create table if not exists comment (body text, id text, replyid text, userid text, timestamp DATETIME)")
+    c.execute("create table if not exists post (title text NOT NULL, body text NOT NULL, postid INTEGER PRIMARY KEY, userid text, timestamp DATETIME)")
+    c.execute("create table if not exists comment (body text NOT NULL, id INTEGER PRIMARY KEY, postid int, userid text, timestamp DATETIME)")
     c.execute("create table if not exists user (username text, password text, id text)")
     conn.commit()
 
-def addPost(body, postid, userid):
+def addPost(title, body, userid):
     conn = sqlite3.connect("bloginator9000.db")
     c = conn.cursor()
-    c.execute("insert into post values ('{}', '{}', '{}', datetime(CURRENT_TIMESTAMP))".format(body, postid, userid))
+    c.execute("insert into post values ('{}', '{}', NULL, '{}', datetime(CURRENT_TIMESTAMP))".format(title, body, userid))
     conn.commit()
 
 def getPosts():
     conn = sqlite3.connect("bloginator9000.db")
     c = conn.cursor()
-    c.execute("select rowid,* from post order by timestamp")
+    c.execute("select * from post order by timestamp")
     data = c.fetchall()
     data.reverse()
-    data = [dict(zip(['rowid','blogtext','postid','username','date'], each)) for each in data]
+    data = [dict(zip(['title','blogtext','postid','username','date'], each)) for each in data]
     return data
 
 def getPost(key, postid):
     conn = sqlite3.connect("bloginator9000.db")
     c = conn.cursor()
-    c.execute("select rowid,* from post where {} = '{}'".format(key,postid))    
-    #c.execute("select rowid,* from post where " + key + " = '" + postid + "'")
+    c.execute("select * from post where {} = '{}'".format(key,postid))
     data = c.fetchall()
-    data = [dict(zip(['rowid','blogtext','postid','username','date'], each)) for each in data]
+    data = [dict(zip(['title','blogtext','postid','username','date'], each)) for each in data]
     return data
 
-def editPost(newtext, postid):
+def addComment(body, replyid, userid):
     conn = sqlite3.connect("bloginator9000.db")
     c = conn.cursor()
-    c.execute("update post set body='{}' where id='{}'".format(newtext, postid))
-    return c.fetchall()
-
-def addComment(body, commentid, replyid, userid):
-    conn = sqlite3.connect("bloginator9000.db")
-    c = conn.cursor()
-    c.execute("insert into comment values ('{}', '{}', '{}', '{}', datetime(CURRENT_TIMESTAMP))".format(body, commentid, replyid, userid))
+    c.execute("insert into comment values ('{}', NULL, '{}', '{}', datetime(CURRENT_TIMESTAMP))".format(body, replyid, userid))
     conn.commit()
 
 def getComments(key, postid):
     conn = sqlite3.connect("bloginator9000.db")
     c = conn.cursor()
-    #c.execute("select rowid,* from post where {} = '{}'".format(key,postid))    
     c.execute("select * from comment where {} = '{}' order by timestamp".format(key,postid))
     data = c.fetchall()
     data.reverse()
     data = [dict(zip(['commenttext','commentid','postid','username','date'], each)) for each in data]    
-    print data
     return data
 
-def editComment(newtext, commentid):
-    conn = sqlite3.connect("bloginator9000.db")
-    c = conn.cursor()
-    c.execute("update comment set body='{}' where id='{}'".format(newtext, commentid))
-    data = c.fetchone()
-    conn.commit()
-    
 def newUser(username, password):
     conn = sqlite3.connect("bloginator9000.db")
     c = conn.cursor()
     isTaken = "SELECT username FROM user WHERE username=\"%s\" LIMIT 1" % (username)
     c.execute(isTaken)
     data = c.fetchone()
-    print "B"
     if data is None :
-        print "A"
         m = hashlib.sha224(password)
         u = hashlib.sha224(username)
         query = "INSERT INTO user VALUES (\"%s\", \"%s\", \"%s\")" % (username, m.hexdigest(), u.hexdigest())
